c34a1082bbc18a8d9e8d8567eb9d55f76f84b57a
Jarred Capellman <jcapellman@hotmail.com>
2018-06-10 16:05:50 -0700

    Added: Continued work on styling and starting work on displaying artists listing

--- src/GroovyMusic/ViewModels/MainPageViewModel.cs
+++ src/GroovyMusic/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace GroovyMusic.ViewModels
+﻿using System.Collections.ObjectModel;
+
+using GroovyMusic.Models;
+
+namespace GroovyMusic.ViewModels
 {
     public class MainPageViewModel : BaseViewModel
     {
+        private ObservableCollection<Artists> _artists;
 
+        public ObservableCollection<Artists> Artists
+        {
+            get => _artists;
+            set { _artists = value; OnPropertyChanged(); }
+        }
+
+        public async void LoadData()
+        {
+            Artists = new ObservableCollection<Artists>(await App.Database.GetArtistsAsync());
+        }
     }
 }
--- src/GroovyMusic/Views/MainPage.xaml.cs
+++ src/GroovyMusic/Views/MainPage.xaml.cs
@@ -1,12 +1,25 @@
-﻿using Xamarin.Forms;
+﻿using GroovyMusic.ViewModels;
+
+using Xamarin.Forms;
 
 namespace GroovyMusic.Views
 {
 	public partial class MainPage : ContentPage
 	{
+	    private MainPageViewModel viewModel => (MainPageViewModel) BindingContext;
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+		    BindingContext = new MainPageViewModel();
 		}
+
+	    protected override void OnAppearing()
+	    {
+	        base.OnAppearing();
+
+            viewModel.LoadData();
+	    }
 	}
 }
